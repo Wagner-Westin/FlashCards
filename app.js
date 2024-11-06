4f0f5c124113293d367cd5527792caffd91d5366
WAGNER WESTIN ROCHA <160676809+Wagner-Westin@users.noreply.github.com>
2024-11-06 22:20:09 +0000

    mud2s

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-function criaCartao(categoria, pergunta, resposta){
+function criaCartao(categoria, pergunta, resposta) {
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
-    cartao.innerName = '
+    cartao.innerName = `
     <div class="cartao__conteudo">
     <h3>${categoria}</h3>
     <div class="cartao__conteudo_pergunta">
         <p>${pergunta}</p>
     </div>
     </div>
+    `
+    let container = document.ElementById('Container')
+    container.appendChild(cartao)
 }
